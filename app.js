276ea77a8a1bb9df9a1c8388387aa7db2f3dcd99
Roberto <roberto.arranz.93@gmail.com>
2017-12-15 00:26:17 +0100

    mongoport

--- app.js
+++ app.js
@@ -13,8 +13,17 @@ app.use(express.static(path.join(__dirname, 'dist')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 mongoose.Promise = global.Promise;
+var options = {
+  useMongoClient: true,
+  autoIndex: false, // Don't build indexes
+  reconnectTries: Number.MAX_VALUE, // Never stop trying to reconnect
+  reconnectInterval: 500, // Reconnect every 500ms
+  poolSize: 10, // Maintain up to 10 socket connections
+  // If not connected, return errors immediately rather than waiting for reconnect
+  bufferMaxEntries: 0
+};
 
-mongoose.connect('mongodb://localhost:27017/cscfutsaldb', function(err, res) {
+mongoose.connect('mongodb://localhost:27017/cscfutsaldb', options, function(err, res) {
   if(err) throw err;
   console.log('Connected to Database');
 });
