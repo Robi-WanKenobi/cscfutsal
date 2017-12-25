b026fd93b59fe63b3ad03ff3826291837f6bb4e6
Roberto <roberto.arranz.93@gmail.com>
2017-12-25 12:31:28 +0100

    visual updates, banner

--- src/app/Components/Admin/admin-add-cronica/admin-add-cronica.component.ts
+++ src/app/Components/Admin/admin-add-cronica/admin-add-cronica.component.ts
@@ -14,7 +14,11 @@ export class AdminAddCronicaComponent implements OnInit {
   jugadores: any;
   jornada: number;
   equipo: string;
-  partido: {};
+  partido: {
+    visitante: string,
+    local: string,
+    resultado: string
+  };
   cronica_id: any;
   goleadores: any[];
 
@@ -88,7 +92,9 @@ export class AdminAddCronicaComponent implements OnInit {
 
   getSeniorAPartido(jornada) {
     this.partidosService.getPartidoSeniorA(jornada).then((res) => {
-      this.partido = res;
+      this.partido.visitante = res['visitante'];
+      this.partido.local = res['local'];
+      this.partido.resultado = res['resultado'];
     }, (err) => {
       console.log(err);
     });
@@ -96,7 +102,9 @@ export class AdminAddCronicaComponent implements OnInit {
 
   getSeniorBPartido(jornada) {
     this.partidosService.getPartidoSeniorB(jornada).then((res) => {
-      this.partido = res;
+      this.partido.visitante = res['visitante'];
+      this.partido.local = res['local'];
+      this.partido.resultado = res['resultado'];
     }, (err) => {
       console.log(err);
     });
@@ -104,7 +112,9 @@ export class AdminAddCronicaComponent implements OnInit {
 
   getSeniorCPartido(jornada) {
     this.partidosService.getPartidoSeniorC(jornada).then((res) => {
-      this.partido = res;
+      this.partido.visitante = res['visitante'];
+      this.partido.local = res['local'];
+      this.partido.resultado = res['resultado'];
     }, (err) => {
       console.log(err);
     });
@@ -112,7 +122,9 @@ export class AdminAddCronicaComponent implements OnInit {
 
   getJuvenilAPartido(jornada) {
     this.partidosService.getPartidoJuvenilA(jornada).then((res) => {
-      this.partido = res;
+      this.partido.visitante = res['visitante'];
+      this.partido.local = res['local'];
+      this.partido.resultado = res['resultado'];
     }, (err) => {
       console.log(err);
     });
@@ -120,7 +132,9 @@ export class AdminAddCronicaComponent implements OnInit {
 
   getJuvenilBPartido(jornada) {
     this.partidosService.getPartidoJuvenilB(jornada).then((res) => {
-      this.partido = res;
+      this.partido.visitante = res['visitante'];
+      this.partido.local = res['local'];
+      this.partido.resultado = res['resultado'];
     }, (err) => {
       console.log(err);
     });
@@ -128,7 +142,9 @@ export class AdminAddCronicaComponent implements OnInit {
 
   getJuvenilCPartido(jornada) {
     this.partidosService.getPartidoJuvenilC(jornada).then((res) => {
-      this.partido = res;
+      this.partido.visitante = res['visitante'];
+      this.partido.local = res['local'];
+      this.partido.resultado = res['resultado'];
     }, (err) => {
       console.log(err);
     });
