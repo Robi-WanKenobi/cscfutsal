043707708ff385713988f78c04fecd4342106bc5
Ben Kenobi <roberto.arranz.93@gmail.com>
2019-01-03 09:46:57 +0100

    fix admin session

--- src/app/Layouts/admin/admin.component.ts
+++ src/app/Layouts/admin/admin.component.ts
@@ -10,14 +10,13 @@ export class AdminComponent implements OnInit {
   
   constructor(private router: Router) { }
 
-  ngOnInit() {
-    let win = (window as any);
-      if(win.location.search !== '?logged' ) {
-          win.location.search = '?logged';
+    ngOnInit() {
+      let win = (window as any);
+      if(win.location.search !== '?logged=csc_admin' ) {
+          this.router.navigate(['/admin'], { queryParams: { logged: 'csc_admin' } });
           win.location.reload();
       }
-  }
-
+    }
   
      toPlantillas() {
        this.router.navigate(['/admin-plantilles']);
